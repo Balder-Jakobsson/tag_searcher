bc37be2bcb5576328f991f7b7c6da0f911e3b985
Jonatan Wästlund <jonte.w1@gmail.com>
2019-04-17 23:39:56 +0200

    Rough beginning

--- src/main/gui.py
+++ src/main/gui.py
@@ -1,6 +1,9 @@
 from tkinter import *
+from checkbox import *
 
 base = Tk()
 label = Label(base, text = "eat shit")
+check_box = Check_box(base)
+check_box.canvas.pack()
 label.pack()
 base.mainloop()
